c8d2895e1046088e6a6226ae8a73bbdda552143d
yiheng-aug30 <63427663+yiheng-aug30@users.noreply.github.com>
2024-08-15 21:42:40 -0400

    Create 3.1.create_mask_file.sh

---
+++ 3.1.create_mask_file.sh
@@ -0,0 +1,13 @@
+### prepare mask file
+path_to_annotation="/annotation_file"
+
+##pLoF+alphamissense
+mask_name="pLOF_alpha_missense"
+echo "M_pLOF pLoF" > "${path_to_annotation}"/${mask_name}_masks.txt
+echo "M_pLOF_and_alpha_missense pLoF,likely_pathogenic" >> "${path_to_annotation}"/${mask_name}_masks.txt
+echo "alpha_missense likely_pathogenic" >> "${path_to_annotation}"/${mask_name}_masks.txt
+
+##pLoF+5 methods missense
+mask_name="pLOF_missense"
+echo "pLOF_and_55missense pLoF,missense.5in5" > "${path_to_annotation}"/${mask_name}_masks.txt
+echo "pLOF_and_55_15missense pLoF,missense.5in5,missense.1in5" >> "${path_to_annotation}"/${mask_name}_masks.txt
